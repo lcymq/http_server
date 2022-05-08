89105298144fd79e229a77a4b7a752f236daf9f7
ClorisMoQi <748709762@qq.com>
2022-05-08 15:39:49 -0700

    update http

--- client.http.go
+++ client.http.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://127.0.0.1:9090/posts/Go/15_socket/")
+	resp, err := http.Get("http://127.0.0.1:9090/testhttp/")
 	if err != nil {
 		fmt.Println("get url failed, err: %v\n", err)
 		return
@@ -17,4 +18,14 @@ func main() {
 		fmt.Println("read resp.Body failed, err:", err)
 	}
 	fmt.Println(string(b))
+
+	data := url.Values{}
+	urlObj, _ := url.Parse("http://127.0.0.1:9000/xxx/")
+	data.Set("name", "moqi")
+	data.Set("age", "16")
+	queryStr := data.Encode()
+	fmt.Println(queryStr)
+	urlObj.RawQuery = queryStr
+	req, err := http.NewRequest("GET", urlObj.String(), nil)
+	resp, err = http.DefaultClient.Do(req)
 }
--- server.http.go
+++ server.http.go
@@ -14,7 +14,15 @@ func f1(w http.ResponseWriter, r *http.Request) {
 	w.Write(file)
 }
 
+func f2(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	fmt.Println(r.Method)
+	fmt.Println(ioutil.ReadAll(r.Body))
+	w.Write([]byte("ok"))
+}
+
 func main() {
 	http.HandleFunc("/posts/Go/15_socket/", f1)
+	http.HandleFunc("/testhttp/", f2)
 	http.ListenAndServe("127.0.0.1:9090", nil)
 }
